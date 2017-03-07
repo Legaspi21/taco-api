294917e1f95a553da90b8195085a1b3571dc9043
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-06 18:42:46 -0800

    Add :taco: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Taco.create({title: "Fried Avocado", description: "Fried, creamy, crunchy, avocado-y goodness, shall we? These yummy tacos are super easy to make and they’re perfect for Meatless Monday, Taco Tuesday, or Get In Mah Belly Day.", uri: "http://i63.tinypic.com/1692h3t.jpg", price: 299})
+Taco.create({title: "Blackberry Chicken", description: "Shredded Hoisin-Blackberry Chicken Tacos with Crunchy Slaw. The chicken is cooked in the slow cooker until it’s so tender that it basically falls apart with no effort.", uri: "http://i66.tinypic.com/20qz2g1.jpg", price: 349})
+Taco.create({title: "Cilantro Lime Shrimp", description: "Although the cilantro lime shrimp would be good in just about everything, they are particularly nice in these fresh summery tacos! ", uri: "http://i67.tinypic.com/dbji54.jpg", price: 399})
+Taco.create({title: "Mango Mahi-Mahi", description: "Mahi-Mahi Fish Tacos with Mango and Avocado Salsa. If your not too familiar with mahi-mahi it’s a white fish that is a little firmer and perfect for fish tacos!", uri: "http://i64.tinypic.com/vnnt74.jpg", price: 499})
+Taco.create({title: "Wild Caught Lobster", description: "Fresh lobster with sweet mango, refreshing cucumber, and creamy avocado with a hint of lime, topped with bright cilantro—it's the ultimate summer taco!", uri: "http://i65.tinypic.com/2ag29o3.jpg", price: 599})
+Taco.create({title: "The Vegan ", description: "These Meatless wonders will have you feeling great about your new years resolution. Mushroom and vegan beef crumbles with avocado and plenty of cilantro. Yummy!", uri: "http://i66.tinypic.com/293czme.jpg", price: 299})
+Taco.create({title: "Carne Asada", description: "The undisputed king of the taco world. Classic with a twist... of lime! Flank steak marinated in our signature Mojo with homemade pico de gallo. Enough said.", uri: "http://i66.tinypic.com/25f7gab.jpg", price: 349})
